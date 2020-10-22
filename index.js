181ec4977053c5ea90c3509934b51b386d46ccf4
Alexander Swed <lexswed@gmail.com>
2020-10-22 12:57:12 +0200

    fix: return trailing comma rule, update deps

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ module.exports = {
     'prettier/react',
     'prettier/standard',
     'standard',
-    'standard-jsx'
+    'standard-jsx',
   ],
   plugins: ['jest'],
   env: {
@@ -18,30 +18,31 @@ module.exports = {
     es6: true,
     jest: true,
     node: true,
-    'jest/globals': true
+    'jest/globals': true,
   },
   parserOptions: {
     ecmaFeatures: {
-      generators: true
-    }
+      generators: true,
+    },
   },
   rules: {
     'import/order': [
       'warn',
       {
-        'newlines-between': 'always'
-      }
+        'newlines-between': 'always',
+      },
     ],
     'import/export': 'warn',
     'jest/no-focused-tests': 'error',
     'no-console': 'warn',
     'prefer-template': 'warn',
     'react/prop-types': 'error',
-    'react-hooks/exhaustive-deps': 'error'
+    'react-hooks/exhaustive-deps': 'error',
+    'comma-dangle': ['error', 'always-multiline'],
   },
   settings: {
     react: {
-      version: 'detect'
-    }
-  }
+      version: 'detect',
+    },
+  },
 }
